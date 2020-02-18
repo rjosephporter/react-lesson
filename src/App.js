2895b3e3a082e995777c72c01efdb8ba02376347
Richard Joseph Porter <richard_porter@ligph.com>
2020-02-18 15:12:08 +0800

    #38

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-      <Person name="Chuck" age="31" />
-      <Person name="Mariel" age="22">My Hobbies: Eating</Person>
-      <Person name="Penny" age="1" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Chuck', age: 31 },
+      { name: 'Mariel', age: 22 },
+      { name: 'Penny', age: 1 }
+    ]
+  }
+  render() { 
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <button>Switch name</button>
+        <Person name={ this.state.persons[0].name } age={this.state.persons[0].age} />
+        <Person name={ this.state.persons[1].name } age={this.state.persons[1].age}>My Hobbies: Eating</Person>
+        <Person name={ this.state.persons[2].name } age={this.state.persons[2].age} />
+      </div>
+    )
+  }
 }
 
 export default App;
