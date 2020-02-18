4e0c3dd52376b9a212d38272526febb485efb2d7
Richard Joseph Porter <richard_porter@ligph.com>
2020-02-18 14:56:51 +0800

    #36

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
+      <Person name="Chuck" age="31" />
+      <Person name="Mariel" age="22">My Hobbies: Eating</Person>
+      <Person name="Penny" age="1" />
     </div>
   );
 }
