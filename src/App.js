504de28ffe95405cfc70b1b6bef23de7fc3d4dea
Richard Joseph Porter <richard_porter@ligph.com>
2020-02-19 13:57:19 +0800

    #56

--- src/App.js
+++ src/App.js
@@ -49,17 +49,9 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={ this.state.persons[0].name } 
-            age={ this.state.persons[0].age }
-            changed={ this.nameChangeHandler } />
-          <Person 
-            name={ this.state.persons[1].name } 
-            age={ this.state.persons[1].age }
-            click={ this.switchNameHandler.bind(this, 'Chuck 2') }>My Hobbies: Eating</Person>
-          <Person 
-            name={ this.state.persons[2].name } 
-            age={ this.state.persons[2].age } />
+          { this.state.persons.map(person => {
+            return <Person name={ person.name } age={ person.age } />
+          })}
         </div>
       )
     }
