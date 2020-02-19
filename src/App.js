74e342e129d8167fb4fa332037a20a1534cfdb3a
Richard Joseph Porter <richard_porter@ligph.com>
2020-02-19 11:47:01 +0800

    #49

--- src/App.js
+++ src/App.js
@@ -31,10 +31,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hello world!</h1>
-        <button onClick={ (event) => this.switchNameHandler('Chuck 1') }>Switch name</button>
+        <button 
+          onClick={ (event) => this.switchNameHandler('Chuck 1') }
+          style={ style }>Switch name</button>
         <Person 
           name={ this.state.persons[0].name } 
           age={ this.state.persons[0].age }
