72ae00bc92105161de134e1ab9ecbb1262e1ca9c
Richard Joseph Porter <richard_porter@ligph.com>
2020-02-21 17:01:48 +0800

    #76

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }    
-`
+import personClasses from './Person.module.css'
 
 const person = (props) => {
     return (
-        <StyledDiv className="Person">
+        <div className={ personClasses.Person }>
             <p onClick={ props.click }>I'm { props.name } and I am { props.age } years old!</p>
             <p>{ props.children }</p>
             <p><input type="text" onChange={ props.changed } value={ props.name } /></p>
             <small><button onClick={ props.delete }>Delete</button></small>
-        </StyledDiv>
+        </div>
     )
 }
 
